b0edaf39b8c450783a7338fe9ef8966d3efafea1
tls1235 <yotam.mini3@gmail.com>
2026-01-26 21:19:49 +0200

    added some zsh init files

---
+++ .functions.sh
@@ -0,0 +1,58 @@
+nix-search() {
+nix search nixpkgs $1 --json | jq -r '.[] | "\(.pname): \(.description)"' | grep -i $1
+}
+lls() {
+	ls -ah $2 | grep $1
+}
+nix-update() {
+	nix profile upgrade --all --refresh --impure
+}
+nix-add() {
+	nix profile add nixpkgs#"$1" --impure 
+}
+hytale() {
+	#!/bin/zsh
+	PLACEHOLDER=($(echo "$1" | tr ',' '\ '))
+	WORLDSFIX=()
+	WORLDSSAVE=()
+	for f in "${PLACEHOLDER[@]}"; do
+		WORLDSSAVE+=(~/.var/app/com.hypixel.HytaleLauncher/data/Hytale/UserData/Saves/"$f")
+		WORLDSFIX+=(~/important/hytale-backups/backup-LATEST/"$f")
+	done
+	if [ $2 = fix ]; then
+	cp -r "${WORLDSFIX[@]}" ~/.var/app/com.hypixel.HytaleLauncher/data/Hytale/UserData/Saves/
+elif [ $1 = save ]; then
+	cp -r ~/.var/app/com.hypixel.HytaleLauncher/data/Hytale/UserData/Saves/* ~/important/hytale-backups/backup-OUTDATED/
+	mv ~/important/hytale-backups/backup-OUTDATED ~/important/hytale-backups/placeholder
+	mv ~/important/hytale-backups/backup-LATEST ~/important/hytale-backups/backup-OUTDATED
+	mv ~/important/hytale-backups/placeholder ~/important/hytale-backups/backup-LATEST
+	exit
+elif [ $1 = fix ]; then
+	cp -r ~/important/hytale-backups/backup-LATEST/* ~/.var/app/com.hypixel.HytaleLauncher/data/Hytale/UserData/Saves/
+	exit
+elif [[ $1 = "" ]]; then
+	flatpak run com.hypixel.HytaleLauncher & disown
+	exit
+	fi
+	flatpak run com.hypixel.HytaleLauncher & disown
+	sleep 300
+	while true; do
+	time=0
+	cp -r "${WORLDSSAVE[@]}" ~/important/hytale-backups/backup-OUTDATED/
+	mv ~/important/hytale-backups/backup-OUTDATED ~/important/hytale-backups/placeholder
+	mv ~/important/hytale-backups/backup-LATEST ~/important/hytale-backups/backup-OUTDATED
+	mv ~/important/hytale-backups/placeholder ~/important/hytale-backups/backup-LATEST
+	until [ "$time" -eq "30" ]; do
+		sleep 10
+		if ! pkill -0 HytaleClient; then
+			cp -r "${WORLDSSAVE[@]}" ~/important/hytale-backups/backup-OUTDATED/
+			mv ~/important/hytale-backups/backup-OUTDATED ~/important/hytale-backups/placeholder
+			mv ~/important/hytale-backups/backup-LATEST ~/important/hytale-backups/backup-OUTDATED
+			mv ~/important/hytale-backups/placeholder ~/important/hytale-backups/backup-LATEST
+			exit
+		else
+			(( time += 1 ))
+		fi
+	done
+done
+}
---
+++ .zshrc
@@ -0,0 +1,36 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+source /usr/share/cachyos-zsh-config/cachyos-config.zsh
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+HISTSIZE=10000
+SAVEHIST=10000
+ZSH_AUTOSUGGEST_STRATEGY=completion
+setopt appendhistory
+setopt sharehistory
+setopt nobeep
+setopt interactivecomments
+setopt AUTO_LIST
+setopt AUTO_MENU
+setopt ALWAYS_TO_END
+unsetopt COMPLETE_IN_WORD
+unsetopt CORRECT
+unsetopt CORRECT_ALL
+bindkey -e
+autoload -Uz compinit
+compinit
+zstyle ':completion:*' matcher-list \
+  'm:{a-z}={A-Z}' \
+  'r:|[._-]=* r:|=*' \
+  'l:|=* r:|=*'
+alias ll='ls -ah'
+alias nix-gc='nix-collect-garbage -d'
+source .functions.sh
+export NIXPKGS_ALLOW_UNFREE=1
